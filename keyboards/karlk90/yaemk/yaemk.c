c5949f98b1178a5f6bb9aa1be75290e535650c25
amonwong <amonwong91@gmail.com>
2022-02-22 17:52:04 +0800

    Update yaemk.c

--- keyboards/karlk90/yaemk/yaemk.c
+++ keyboards/karlk90/yaemk/yaemk.c
@@ -62,33 +62,33 @@ bool oled_task_user(void) {
 }
 #endif
 
-bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-#if defined(RGB_MATRIX_ENABLE)
-        case RGB_TOG:
-            if (record->event.pressed) {
-                switch (rgb_matrix_get_flags()) {
-                    case LED_FLAG_ALL: {
-                        rgb_matrix_set_flags(LED_FLAG_KEYLIGHT);
-                    } break;
-                    case LED_FLAG_KEYLIGHT: {
-                        rgb_matrix_set_flags(LED_FLAG_UNDERGLOW);
-                    } break;
-                    case LED_FLAG_UNDERGLOW: {
-                        rgb_matrix_set_flags(LED_FLAG_NONE);
-                        rgb_matrix_disable();
-                    } break;
-                    default: {
-                        rgb_matrix_set_flags(LED_FLAG_ALL);
-                        rgb_matrix_enable();
-                    } break;
-                }
-            }
-            return false;
-#endif
-    }
-    return process_record_user(keycode, record);
-}
+// bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
+//     switch (keycode) {
+// #if defined(RGB_MATRIX_ENABLE)
+//         case RGB_TOG:
+//             if (record->event.pressed) {
+//                 switch (rgb_matrix_get_flags()) {
+//                     case LED_FLAG_ALL: {
+//                         rgb_matrix_set_flags(LED_FLAG_KEYLIGHT);
+//                     } break;
+//                     case LED_FLAG_KEYLIGHT: {
+//                         rgb_matrix_set_flags(LED_FLAG_UNDERGLOW);
+//                     } break;
+//                     case LED_FLAG_UNDERGLOW: {
+//                         rgb_matrix_set_flags(LED_FLAG_NONE);
+//                         rgb_matrix_disable();
+//                     } break;
+//                     default: {
+//                         rgb_matrix_set_flags(LED_FLAG_ALL);
+//                         rgb_matrix_enable();
+//                     } break;
+//                 }
+//             }
+//             return false;
+// #endif
+//     }
+//     return process_record_user(keycode, record);
+// }
 
 /* Fast Matrix Port Scanning */
 
